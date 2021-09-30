1df93470557a6b70046a08a4b91e614367ec3563
houyu0808 <82366737+houyu0808@users.noreply.github.com>
2021-09-30 10:26:12 +0800

    Delete online_learning_platform_back/src/main/java/com/houyu/online_learning_platform/utils/globalErrorResponse directory

--- online_learning_platform_back/src/main/java/com/houyu/online_learning_platform/utils/globalErrorResponse/myControllerAdvice.java
+++
@@ -1,8 +0,0 @@
-package com.houyu.online_learning_platform.utils.globalErrorResponse;
-
-import org.springframework.web.bind.annotation.ControllerAdvice;
-
-@ControllerAdvice
-public class myControllerAdvice {
-
-}
