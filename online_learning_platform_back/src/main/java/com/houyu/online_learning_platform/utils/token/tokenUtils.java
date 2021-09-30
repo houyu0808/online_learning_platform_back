170d73c0639c301bbec060d5d8dd1a2e04cc2a59
houyu0808 <82366737+houyu0808@users.noreply.github.com>
2021-09-30 10:22:09 +0800

    update

--- online_learning_platform_back/src/main/java/com/houyu/online_learning_platform/utils/token/tokenUtils.java
+++ online_learning_platform_back/src/main/java/com/houyu/online_learning_platform/utils/token/tokenUtils.java
@@ -3,6 +3,7 @@ package com.houyu.online_learning_platform.utils.token;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.exceptions.JWTDecodeException;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.houyu.online_learning_platform.utils.responseMessage.ResponseMessage;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
@@ -11,15 +12,13 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Date;
 
 @Configuration
 public class tokenUtils implements HandlerInterceptor {
 
-    public static final String SECRET = "JKKLJOoasdlfj";
-    @Value(value = "${app.secret}")
-    private String secret;
-    @Value(value = "${app.auther}")
+    @Value("${app.auther}")
     private String auther;
     //拦截Request 校验token
     @Override
@@ -30,36 +29,43 @@ public class tokenUtils implements HandlerInterceptor {
             return true;
         }else{
             try{
-                return decodeToken(token);
+                return decodeToken(token,response);
             }catch (Exception e){
+                falseResult(response);
                 return false;
             }
         }
     }
     //解析token
-    public boolean decodeToken(String token){
+    public boolean decodeToken(String token,HttpServletResponse response) throws Exception{
         if(token == null){
+            falseResult(response);
             return false;
         }else{
             try {
                 DecodedJWT jwt = JWT.decode(token);
-
-                String algorithm = jwt.getAlgorithm(); //获取算法类型
+                Date nowTime = new Date();
                 String type = jwt.getType();    //获取token类型
                 String issuer = jwt.getIssuer();    //获取token发布者
                 Date expiresAt = jwt.getExpiresAt(); //获取token过期时间
-                Date issuedAt = jwt.getIssuedAt();  // 获取token生产日期
-
-                System.out.println(algorithm);  //=>    HS256
-                System.out.println(type);       //=>    JWT
-                System.out.println(issuer);     //=>    auth0
-                System.out.println(expiresAt);  //=>    Sat Jan 11 22:25:13 CST 2020
-                System.out.println(issuedAt);   //=>    Sat Jan 11 20:25:13 CST 2020
-                return true;
+                if(issuer.equals("houyu")&&expiresAt.after(nowTime)){
+                    return true;
+                }else{
+                    falseResult(response);
+                    return false;
+                }
             } catch (JWTDecodeException exception){
                 //无效的 token
+                falseResult(response);
                 return false;
             }
         }
+    }
+    //返回错误信息
+    public void falseResult(HttpServletResponse response) throws IOException{
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json; charset=utf-8");
+        ObjectMapper objectMapper = new ObjectMapper();
+        response.getWriter().println(objectMapper.writeValueAsString(ResponseMessage.error("token验证失败!")));
     }
 }
